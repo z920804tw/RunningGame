8d92d82dd734f727c015e15d51be23f1323bf2a1
z920804tw <129352148+z920804tw@users.noreply.github.com>
2025-03-06 16:15:05 +0800

    2025/3/6
    1.小更新

--- Assets/Scripts/PlatformManager.cs
+++ Assets/Scripts/PlatformManager.cs
@@ -1,28 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlatformManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject[] platformPrefabs;
-    public Transform spawnPos;
+    public float platformLength;
+    [SerializeField] GameObject preObj;
     
     void Start()
     {
-
+        preObj=transform.GetChild(0).gameObject;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-
-    }
+    
     public void SpawnNext()
     {
+        Vector3 spawnPos=new Vector3(preObj.transform.position.x,preObj.transform.position.y,
+            preObj.transform.position.z+platformLength);
+
+        spawnPos.z=(int)spawnPos.z;
+        Debug.Log(spawnPos);
         int rnd = Random.Range(0, platformPrefabs.Length);
-        GameObject pf = Instantiate(platformPrefabs[rnd],spawnPos.position, Quaternion.identity);
+        GameObject pf = Instantiate(platformPrefabs[rnd],spawnPos, Quaternion.identity);
         pf.transform.SetParent(this.transform);
         pf.name = $"Platform";
+        
+        preObj=pf;
     }
 }
